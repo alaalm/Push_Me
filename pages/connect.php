ea8517e853fc33ce0f14d990bcfa7c66480cedd0
alaalm <mohdeneee@hotmail.com>
2018-08-03 07:30:59 +0300

    debugging message off

--- pages/connect.php
+++ pages/connect.php
@@ -1,5 +1,5 @@
 <?php
-//error_reporting(0);
+error_reporting(0);
 $url = "eu-cdbr-west-02.cleardb.net";
 $database= "heroku_b5e473d9b007cb2";
 $username="b7c9018088c3d9";
@@ -11,7 +11,7 @@ $conn= mysql_connect($url,$username,$password,$database);
 if (!$conn){
 		die('Could not connect :'.mysql_error());
 }else  
-echo "Connected successfully";
+//echo "Connected successfully";
 session_start();
-echo "session_start";
+//echo "session_start";
 ?>
