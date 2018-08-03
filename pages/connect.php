f87a64dd48374810b4db5ac44c2ee6abc6ef3ec7
alaalm <mohdeneee@hotmail.com>
2018-08-03 07:32:04 +0300

    l

--- pages/connect.php
+++ pages/connect.php
@@ -4,7 +4,7 @@ $url = "eu-cdbr-west-02.cleardb.net";
 $database= "heroku_b5e473d9b007cb2";
 $username="b7c9018088c3d9";
 $password ="54964bdb";
-echo "<center>";
+//echo "<center>";
 
 $conn= mysql_connect($url,$username,$password,$database);
 
